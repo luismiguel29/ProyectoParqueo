c9d76a34ec9b6c8ac92ad9051e9362e9824aaa7d
luismiguel29 <rojashuallcoluismiguel29@gmail.com>
2023-04-07 12:55:47 -0400

    Prueba vista y ruta

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PruebaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::controller(PruebaController::class)->group(function(){
+    Route::get('/vistaejemplo', 'index');
 });

bfe8c7cede7a4275eb03a5b0bbe4151a50f833f5
ypelaez <yohana.pelaez.cruz@gmail.com>
2023-04-20 15:48:57 -0400

    [UPDATE] Template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,9 @@
         <!-- Fonts -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link  rel="stylesheet" href="{{asset('css/template.css')}}">
-       
-    </head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+      </head>
     <body>
     
         <header class="navbar navbar-sky sticky-top bg-dark flex-md-nowrap p-0 shadow">
@@ -23,23 +24,26 @@
           </button>
           <div class="navbar-nav">
             <div class="menu-header">
+              <div class="container">
                 <ul class="nav flex-row">
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">  
-                      Notificación
+                      <i class="fa-solid fa-bell"></i>
                     </a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">  
-                      Usario
+                      <i class="fa-solid fa-user"></i>
                     </a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">  
-                      Cerrar sesión
+                      <i class="fa-solid fa-power-off"></i>
                     </a>
                   </li>
                 </ul>
+              </div>
+                
           </div>
         </header>
         
@@ -50,42 +54,60 @@
                 <ul class="nav flex-column">
                   <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">  
+                      <i class="fa-solid fa-house pe-2"></i>
                       Inicio
                     </a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#">
                       <span data-feather="file"></span>
+                      <i class="fa-solid fa-users pe-2"></i>
+                      
                       Usuarios
                     </a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#">
                       <span data-feather="shopping-cart"></span>
+                      <i class="fa-solid fa-user pe-2"></i>
                       Cliente
                     </a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#">
                       <span data-feather="users"></span>
+                      <i class="fa-solid fa-square-parking pe-2"></i>
                       Parqueo
                     </a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#">
                       <span data-feather="bar-chart-2"></span>
+                      <i class="fa-solid fa-sliders pe-2"></i>
                       Configuración
+                      <i></i>
+                        
                     </a>
                   </li>
                   
                 </ul>
               </div>
             </nav>
-        
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+              <div class=" py-3 mb-3 border-bottom">
+               <H1>TITULO DE PRUEBA</H1>
+                <!--BODY INIT-->
+                
+   
+                <!--BODY END-->
+              </div>
+            
+            </main>
     
           </div>
         </div>
             <script type="module" src="./js/main.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
+
           </body>
 </html>

40c9160c0a70a86d5aa1339dfeafd585cd19ce01
and312 <andreapelaez436@gmail.com>
2023-04-21 11:21:23 -0400

    add view deleteSite

---
+++ resources/views/deleteSite.blade.php
@@ -0,0 +1,23 @@
+
+<div class="modal fade" id="modal-delete-{{$site->id}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <form action="{{route('sites.destroy', $site->id)}}" method="POST">
+            @csrf
+            @method('DELETE')
+            
+            <div class="modal-content">
+                <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Eliminación de sitios de parqueo</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Desea eliminar el sitio {{$site->numero_espacio}}?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Cerrar</button>
+                    <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>

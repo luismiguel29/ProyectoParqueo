fed31a59b9a6a0b5ed3486ff5086625589fc3c30
and312 <andreapelaez436@gmail.com>
2023-04-21 11:19:45 -0400

    add modal of deleteSite

--- resources/views/listSite.blade.php
+++ resources/views/listSite.blade.php
@@ -4,61 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/listSite.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
 </head>
-<body>
-    <div class="container">
-        <div class="title">
-            <img src="/img/logoP.png" alt="minilogo">
-            <span>Lista de sitios</span>
-        </div>
-        
-        <div class="row">
-            <div class="col-lg-3">
-                <div class="table-responsive">
-                    <table class="table table-striped table-bordered table-condensed" style="width: 100%">
-                        <thead>
-                        <!--<thead class="text-center">-->
-                            <tr>
-                                <th>N°</th>
-                                <th>N° espacio</th>
-                                <th>Ación</th>
-                            </tr>
-                        </thead>
-                        
-                        <tbody>
-                            @foreach($listSites as $site)
-                            <tr>
-                                <td>{{$site->id}}</td>
-                                <td>{{$site->numero_espacio}}</td>
-                                <td>
-                                    <div class="imgAction">
-                                        <div class="bicon icon--blue">
-                                            <i class="fa-solid fa-pen-to-square icon--white"></i>
-                                        </div>
-                                        
-                                        <form action="{{route('sites.destroy', $site->id)}}" method="POST">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="bicon icon--red">
-                                                <i class="fa-solid fa-trash-can icon--white"></i>
-                                            </button>
-                                        </form>
-                                        
-                                    </div>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+
+@extends('welcome')
+@section('content')
+    <section>
+        <div class="container">
+            <div class="title">
+                <div class="bicon icon--dark">
+                    <i class="fa-solid fa-p icon--white"></i>
+                </div>
+                <span>Lista de sitios</span>
+            </div>
+            
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-condensed" style="width: 100%">
+                            <thead>
+                            <!--<thead class="text-center">-->
+                                <tr>
+                                    <th>N°</th>
+                                    <th>N° espacio</th>
+                                    <th>Ación</th>
+                                </tr>
+                            </thead>
+                            
+                            <tbody>
+                                @foreach($listSites as $site)
+                                    <tr>
+                                        <td>{{$site->id}}</td>
+                                        <td>{{$site->numero_espacio}}</td>
+                                        <td>
+                                            <div class="imgAction">
+                                                <div class="bicon icon--blue">
+                                                    <i class="fa-solid fa-pen-to-square icon--white"></i>
+                                                </div>
+                                                
+                                                <button type="submit" class="bicon icon--red" data-bs-toggle="modal" data-bs-target="#modal-delete-{{$site->id}}">
+                                                    <i class="fa-solid fa-trash-can icon--white"></i>
+                                                </button>
+                                                
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    @include('deleteSite')
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-
-</body>
+    </section>
+@endsection
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 </html>

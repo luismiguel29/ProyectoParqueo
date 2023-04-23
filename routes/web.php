32fc66063c027e27e1db89fd8429b8b3688165bd
ypelaez <yohana.pelaez.cruz@gmail.com>
2023-04-23 00:32:46 -0400

    [UPDATE] solution-error

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,9 @@
 use App\Http\Controllers\HorarioController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PruebaController;
-<<<<<<< HEAD
+use App\Http\Controllers\VerParqueoController;
 use App\Http\Controllers\ConfiguracionParqueo\CrearSitioController;
 use App\Http\Controllers\AdmInfoClientes\ClienteController;
-=======
-Use App\Http\Controllers\ConfiguracionParqueo\CrearSitioController;
-Use App\Http\Controllers\VerParqueoController;
->>>>>>> feature/parkingView
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,7 +45,6 @@ Route::controller(PruebaController::class)->group(function(){
 
 Route::resource('/crear', CrearSitioController::class);
 
-<<<<<<< HEAD
 Route::resource('/login', LoginController::class);
 Route::controller(LoginController::class)->group(function(){
     Route::get('/loginhorario', 'store');
@@ -66,8 +61,6 @@ Route::controller(HorarioController::class)->group(function(){
 });
 
 
-=======
 Route::controller(VerParqueoController::class)->group(function(){
     Route::get('/VerParqueo', 'index');
 });
->>>>>>> feature/parkingView

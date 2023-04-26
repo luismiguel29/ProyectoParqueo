d437241127e28d3ba3138e3c7cebbae71241653c
rodolfokp <rodolfokevinperez@gmail.com>
2023-04-26 00:41:36 -0400

    BD

--- app/Http/Controllers/RegisterAdmin/VistaRegisterController.php
+++ app/Http/Controllers/RegisterAdmin/VistaRegisterController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\DB;
 class VistaRegisterController extends Controller
 {
     public function index() {
-        $usuarios = user::select('id', 'tipo', 'nombre', 'apellido', 'telefono', 'correo')
+        $usuarios = User::select('id', 'tipo', 'nombre', 'apellido', 'telefono', 'correo')
         ->where('tipo', '<>' , 'cliente')
         ->get();
 
